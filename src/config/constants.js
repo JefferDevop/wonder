af4873c58754239b9d832a0220043babeac186b1
JefferDevop <somadesarrollo@gmail.com>
2024-10-27 20:21:10 -0500

    The first commit

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+// export const BASE_API = "https://boutiquemonaco.suprainnovations.store"
+export const BASE_API = "https://wonderarthome.suprainnovations.store"
+// export const BASE_API = "http://testing.localhost:8000"
+
+export const SEARCH_HISTORY = "search-history";
+export const CART = "cart_monaco";
+export const TOKEN = "token";
+export const BASE_NAME = "https://res.cloudinary.com/ciberneto/"
